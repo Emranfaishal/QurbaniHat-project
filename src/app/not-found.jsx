49a00d00032a2768b0be46142dabca05777e3132
Emranfaishal <emranfaishalrahat@gmail.com>
2026-05-03 01:51:23 +0600

    nofount add

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,32 +1,14 @@
 import Link from 'next/link';
-import React from 'react';
-
-const ErrorPage = () => {
+const notFound = () => {
     return (
-        <div className='p-20'>
-            <div className=" flex items-center justify-center py-20 bg-gray-50 rounded-3xl border-2  border-gray-200">
-                <div className="text-center">
-                    <h1 className="text-8xl font-extrabold text-gray-800">404</h1>
-
-                    <h2 className="mt-4 text-2xl font-semibold text-gray-700">
-                        Page Not Found
-                    </h2>
-
-                    <p className="mt-2 text-gray-500 max-w-md mx-auto">
-                        Oops! The page you are looking for doesn’t exist or has been moved.
-                        Please check the URL or go back to the homepage.
-                    </p>
-
-                    <div className="mt-6">
-                        <Link
-                            to="/"
-                            className="px-6 py-3 bg-red-500 text-white rounded-lg shadow hover:bg-[#244D3F] transition"
-                        > Go to Home </Link>
-                    </div>
-                </div>
-            </div>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-5xl font-bold text-red-500">404</h1>
+            <p className="text-xl mt-2">Page Not Found</p>
+            <Link href="/" className="mt-4 text-blue-500 underline">
+                Go Back Home
+            </Link>
         </div>
     );
 };
 
-export default ErrorPage;
+export default notFound;
