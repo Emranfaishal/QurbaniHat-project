3363aa0391ef94eaac2870305cbfad943e3309b8
Emranfaishal <emranfaishalrahat@gmail.com>
2026-05-03 00:56:25 +0600

    toast add

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,8 +32,8 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar></Navbar>
         {children}
+        <ToastContainer position="top-center" />
         <Footer></Footer>
-        <ToastContainer />
 
       </body>
     </html>
