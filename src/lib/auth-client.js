8909a23ed91a5e80e31ccb66463d44db6a5b943c
Emranfaishal <emranfaishalrahat@gmail.com>
2026-05-03 14:30:59 +0600

    auth base url add

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    // baseURL: "https://qurbani-hat-project.vercel.app"
-    baseURL: "http://localhost:3000"
+    baseURL: "https://qurbani-hat-project.vercel.app"
+    // baseURL: "http://localhost:3000"
 });
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
